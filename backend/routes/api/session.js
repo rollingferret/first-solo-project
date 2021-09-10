1e5877b0adf412fdb0afcd2a953bff2cd24cf1f4
Peter Joh <johpeter@gmail.com>
2021-09-10 13:18:10 -0600

    almost done

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,11 +1,24 @@
 // backend/routes/api/session.js
 const express = require('express');
 const asyncHandler = require('express-async-handler');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
 const router = express.Router();
+
+const validateLogin = [
+    check('credential')
+      .exists({ checkFalsy: true })
+      .notEmpty()
+      .withMessage('Please provide a valid email or username.'),
+    check('password')
+      .exists({ checkFalsy: true })
+      .withMessage('Please provide a password.'),
+    handleValidationErrors,
+  ];
 
 // Restore session user
 router.get(
@@ -24,6 +37,7 @@ router.get(
 // Log in
 router.post(
     '/',
+    validateLogin,
     asyncHandler(async (req, res, next) => {
       const { credential, password } = req.body;
 
@@ -44,6 +58,16 @@ router.post(
       });
     }),
   );
+
+// fetch request for logging
+//   fetch('/api/session', {
+//     method: 'POST',
+//     headers: {
+//       "Content-Type": "application/json",
+//       "XSRF-TOKEN": `<value of XSRF-TOKEN cookie>`
+//     },
+//     body: JSON.stringify({ credential: '', password: 'password' })
+//   }).then(res => res.json()).then(data => console.log(data));
 
 // Log out
 router.delete(
