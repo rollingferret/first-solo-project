d522c0e39487f3cf32a8fbaddb8874f2f409a8e7
Peter Joh <johpeter@gmail.com>
2021-09-10 11:26:52 -0600

    first push

---
+++ backend/routes/index.js
@@ -0,0 +1,19 @@
+// backend/routes/index.js
+const express = require('express');
+const router = express.Router();
+
+const apiRouter = require('./api');
+router.use('/api', apiRouter);
+// ...
+
+
+router.get('/hello/world', function(req, res) {
+  res.cookie('XSRF-TOKEN', req.csrfToken());
+  res.send('Hello World!');
+});
+
+router.post('/api/test', function(req, res) {
+    res.json({ requestBody: req.body });
+  });
+
+module.exports = router;
