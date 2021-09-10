d6176cb54750868b58d620fe2f3990b94893c269
Peter Joh <johpeter@gmail.com>
2021-09-10 16:40:57 -0600

    repushing

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -17,18 +17,22 @@ router.post('/test', (req, res) => {
 const asyncHandler = require('express-async-handler');
 const { setTokenCookie } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
-router.get('/set-token-cookie', asyncHandler(async (req, res) => {
-  const user = await User.findOne({
-      where: {
-        username: 'Demo-lition'
-      },
-    })
-  setTokenCookie(res, user);
-  return res.json({ user });
-}));
+
+
+// router.get('/set-token-cookie', asyncHandler(async (req, res) => {
+//   const user = await User.findOne({
+//       where: {
+//         username: 'Demo-lition'
+//       },
+//     })
+//   setTokenCookie(res, user);
+//   return res.json({ user });
+// }));
 
 // router.post('/test', function(req, res) {
 //     res.json({ requestBody: req.body });
+//     console.log({ requestBody: req.body });
+
 //   });
 
 // // GET /api/restore-user
